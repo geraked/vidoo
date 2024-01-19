bff9348f57226244f699d9948c203e2f473f320c
Rabist <Rsajedi2020@gmail.com>
2024-01-19 11:00:22 +0330

    Remove notifications

--- scripts/background.js
+++ scripts/background.js
@@ -17,16 +17,9 @@
     }
 
     async function toggleStatus() {
-        await chrome.notifications.clear("vidoo-toggle");
         let obj = await chrome.storage.local.get({ vidooIsOn: true });
         await chrome.storage.local.set({ vidooIsOn: !obj.vidooIsOn });
         await updateIcon();
-        await chrome.notifications.create("vidoo-toggle", {
-            type: "basic",
-            title: "Vidoo is " + (!obj.vidooIsOn ? "ON" : "OFF"),
-            message: "Reloading the pages is required to apply the changes.",
-            iconUrl: !obj.vidooIsOn ? "/icons/vidoo.png" : "/icons/vidoo-gs.png"
-        });
     }
 
 })();
